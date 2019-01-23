9365f0ab15c06b1ea5d2a246762f742a78c5af98
Alex Elgin <sascha.elgin@gmail.com>
2019-01-23 16:40:07 +0600

    use vee-validate

--- src/components/AddActivityField.vue
+++ src/components/AddActivityField.vue
@@ -1,12 +1,16 @@
 <template>
   <v-form ref="form">
-    <v-text-field v-model="newActivityTitle"
+    <v-text-field
+                  v-model="newActivityTitle"
+                  v-validate="'alpha_spaces|min:3|required'"
+                  :error-messages="errors.collect('newActivityTitle')"
+                  name="newActivityTitle"
+                  data-vv-as="Activity"
                   color="cyan"
                   label="What to do?"
                   @keydown.enter.prevent="add"
-                  :rules="[rules.minLength, rules.unique]"
+                  :rules="[rules.unique]"
                   ref="field"
-                  required
                   append-outer-icon="send"
                   @click:append-outer="add"
     />
@@ -14,7 +18,6 @@
 </template>
 
 <script>
-const MIN_LENGTH = 3
 
 export default {
   name: 'AddActivityField',
@@ -22,25 +25,26 @@ export default {
     return {
       newActivityTitle: '',
       isValid: {
-        minLength: value => (value !== undefined) && (value.length >= MIN_LENGTH),
         unique: value => this.$store.state.activities.filter(activity => activity.title === value).length === 0
       },
       rules: {
-        minLength: value => (
-          this.isValid.minLength(value) || `The activity name should be at least ${MIN_LENGTH} characters long`
-        ),
         unique: value => this.isValid.unique((value || '').trim()) || `The activity is already in the list`
       }
     }
   },
   methods: {
     add () {
-      if (this.isValid.minLength(this.newActivityTitle) && this.isValid.unique(this.newActivityTitle)) {
+      this.$validator.validateAll()
+
+      if (this.isVeeValidationPassed() && this.isValid.unique(this.newActivityTitle)) {
         this.$store.dispatch('add', this.newActivityTitle.trim())
         this.newActivityTitle = ''
         this.$refs.field.blur()
         this.$refs.form.reset()
       }
+    },
+    isVeeValidationPassed () {
+      return Object.values(this.fields).reduce((valid, field) => valid && field.valid, true)
     }
   }
 }
